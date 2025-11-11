2c9e278c35e06c7a0dc0c7efa0f64fbe8bde6277
Rakesh yadav <71434105+Rakeshya@users.noreply.github.com>
2025-11-11 13:57:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -45,7 +45,13 @@ def predict_spam():
     except Exception as e:
         print(traceback.format_exc())
         return jsonify({"error": str(e)}), 500
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
 
 
 if __name__ == "__main__":
     app.run(debug=True)
+
